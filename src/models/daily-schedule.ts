520de93a64eef246078a5e28081d405dd04ae34e
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-08-14 12:06:31 +0530

    feat: :sparkles: add daily-schedule schema

---
+++ src/models/daily-schedule.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import { DailyScheduleSchema } from "typings/schema";
+
+enum Weekdays {
+	monday = "monday",
+	tuesday = "tuesday",
+	wednesday = "wednesday",
+	thursday = "thursday",
+	friday = "friday",
+	saturday = "saturday",
+	sunday = "sunday",
+}
+
+const dailySchedule = new Schema<DailyScheduleSchema>({
+	name: {
+		type: String,
+		required: true,
+		min: 5,
+		max: 40,
+	},
+	day: {
+		type: String,
+		required: true,
+		enum: Weekdays,
+	},
+	routineId: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		ref: "Routine",
+	},
+	exercises: {
+		type: [Schema.Types.ObjectId],
+		required: true,
+		ref: "Exercise",
+	},
+});
+
+export const DailySchema = mongoose.model<DailyScheduleSchema>("DailySchedule", dailySchedule);
--- src/typings/schema/index.ts
+++ src/typings/schema/index.ts
@@ -2,3 +2,4 @@ export * from "./user-schema";
 export * from "./routine-schema";
 export * from "./plan-schema";
 export * from "./holiday-schema";
+export * from "./daily-schedule-schema";
